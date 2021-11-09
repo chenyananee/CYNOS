9b9e6ce4afea3ea5e6a766815224d5038d7f4203
yanan.chen <yanan.chen8@pactera.com>
2021-11-09 17:09:44 +0800

    add list

--- CynOS/cynos_list.c
+++ CynOS/cynos_list.c
@@ -19,5 +19,137 @@ Copyright © 2020 ChenYanan.
 #if USE_CYNOS_LIST
 
 
+//flag=1: pnode will be insert to plisthead->last
+//flag=0: pnode will be insert to plisthead->next
+int CynOSListInsert(void **head,void *insert,char flag)
+{
+  CynOSList_Hanlde *listhead = (CynOSList_Hanlde *)(*((CynOSList_Hanlde **)head));
+  CynOSList_Hanlde *pnode = (CynOSList_Hanlde *)insert;
+  CynOSList_Hanlde *iterator_index = listhead; 
+  if((!(head)) || (!pnode)) return CYNOS_ERR_MEM;
+
+  CynOS_List_Node_Init(pnode);
+  if(!(listhead))
+  {
+    CynOSList_Hanlde **__listaddr =  (CynOSList_Hanlde **)(head);
+    *__listaddr = pnode;
+    return CYNOS_OK;
+  }
+
+  if(flag)
+  {
+    pnode->last = iterator_index->last;
+    pnode->next = iterator_index;
+    iterator_index->last = pnode;
+    if(pnode->last)
+    {
+      pnode->last->next = pnode;
+    }
+  }
+  else
+  {
+    pnode->next = iterator_index->next;
+    pnode->last = iterator_index;
+    iterator_index->next = pnode;
+    if(pnode->next)
+    {
+        pnode->next->last = pnode;
+    }
+  }
+
+  return CYNOS_OK;
+}
+
+
+int CynOSListDelete(void *head,void *rmnode)
+{
+    CynOSList_Hanlde *listhead = head;
+    CynOSList_Hanlde *pnode = rmnode;
+    if((!pnode)) return CYNOS_ERR_MEM;
+
+    if(listhead == pnode)
+    {
+      if(listhead != listhead->next)
+      {
+        listhead = listhead->next;
+      }
+      else
+      {
+        listhead = 0;
+      }
+    }
+
+    if(pnode->next)
+    {
+      pnode->next->last = pnode->last;
+    }
+    
+    if(pnode->last)
+    {
+      pnode->last->next = pnode->next;
+    }
+    
+    return CYNOS_OK;
+}
+
+
+void CynOSListToLoopMode(void *head)
+{
+    CynOSList_Hanlde *listFirst = 0,*listLast = 0;
+    CynOSList_Hanlde *list;
+    CynOS_List_Foreach(head,list)
+
+    if((list->last == list) || (!list->last))
+    {
+      //this is first node in list
+      listFirst = list;
+    }
+
+    if((list->next == list) || (!list->next))
+    {
+      //this is last node in list
+      listLast = list;
+    }
+
+    CynOS_List_ForeachEnd(head)
+
+    if(listFirst)
+    {
+      listFirst->last = listLast;
+    }
+    
+    if(listFirst)
+    {
+      listLast->next = listFirst;
+    } 
+}
+
+/*example*/
+/*
+typedef struct
+{
+   CynOSList_Hanlde list; //must be first
+   int data;
+}list_t;
+
+list_t ttttt[10];
+list_t *lhead=0; 
+
+void app()
+{
+
+  for(int iii=0;iii<10;iii++)
+   {
+      CynOSListInsert((void **)(&lhead),&ttttt[iii],1);
+      ttttt[iii].data = iii;
+   }
+
+   list_t *lout=0;
+   CynOS_List_Foreach(lhead,lout)
+   printf("\r\nlist[%08d]-val = %d",(unsigned int)lout,lout->data);
+   CynOS_List_ForeachEnd(lhead)
+
+}
+*/
 #endif
 
--- CynOS/cynos_list.h
+++ CynOS/cynos_list.h
@@ -7,22 +7,13 @@ extern "C" {
 #endif
 #include "cynos_base.h"
 
-typedef char (*__listfun)(void *arg);
-
-typedef struct 
-{
-    __listfun entry;
-    __listfun exit;
-}_ListFunc;
-
-
 typedef struct __cynos_list
 {
   struct  __cynos_list *next;
   struct  __cynos_list *last;
-  _ListFunc func; 
-
 }CynOSList_Hanlde;
+
+
 
 /*
     do
@@ -31,34 +22,32 @@ typedef struct __cynos_list
         2.do the usr logic
     }while(the next node is not null)
 */
-#define CynOS_List_Iterator_Begin(plisthead,pnode)  \
-CynOSList_Hanlde *iterator_index = plisthead;       \
-do                                                  \
-{                                                   \
-    pnode = iterator_index;                         \
-    iterator_index = iterator_index->next           \
+#define CynOS_List_Foreach(__plisthead,__plistiterator_out)             \
+{                                                                       \
+    CynOSList_Hanlde **__listaddr =  (CynOSList_Hanlde **)(&__plistiterator_out);                 \
+    *__listaddr = (CynOSList_Hanlde *)(__plisthead);                           \
+    do                                                                      \
+    {
+    
+
+
+#define CynOS_List_ForeachEnd(__plisthead)     \
+        if((*__listaddr)->next) {*__listaddr = (*__listaddr)->next;}     \
+    }while(((CynOSList_Hanlde *)(__plisthead) != (*__listaddr)) && ((*__listaddr)->next));  \
 }
 
-
-#define CynOS_List_Iterator_End while(iterator_index != CynOS_NULL)
-
+//break the loop when user need
 #define CynOS_List_Iterator_Break break;
 
-#define CynOS_List_Init(plisthead) \
-plisthead->next = plisthead->last = CynOS_NULL;
-
-//TODO 
-#define CynOS_List_Add(plisthead,pnode) \
-CynOSList_Hanlde *iterator_index = plisthead;       \
-CynOS_List_Init(pnode);                             \
-if(iterator_index == CynOS_NULL)                    \
-plisthead = pnode;                                  \
+//init the list to loop mode
+#define CynOS_List_Node_Init(plisthead)                                              \
+CynOSList_Hanlde *__listiteratorforInit = (CynOSList_Hanlde *)(plisthead);      \
+__listiteratorforInit->next = __listiteratorforInit->last = __listiteratorforInit;
 
 
-
-
-#define CynOS_List_Remove(pnode) \
-
+extern int CynOSListInsert(void **head,void *insert,char flag);
+extern int CynOSListDelete(void *head,void *rmnode);
+extern void CynOSListToLoopMode(void *head);
 
 
 /* C++ detection */
